755efcfabee3e95af5d0c1d6cff1f2add47d5054
StefPY <stefan.mail.sk@gmail.com>
2019-12-11 12:28:00 +0100

    Timer and 3 levels done

--- main.py
+++ main.py
@@ -3,6 +3,8 @@
 import pygame
 from pygame.locals import *
 from source import Array
+import time
+
 
 class MS:
     WHITE = (255, 255, 255)
@@ -11,25 +13,24 @@ class MS:
     W = 805
     H = 600
 
-    pygame.display.set_caption("M   I   N   E   S   W   E   E   P   E   R")
-
-
     def __init__(self):
 
         pygame.init()
-        self.w = MS.W-30 # 775
-        self.h = MS.H-100 # 500
-        self.screen = pygame.display.set_mode((MS.W, MS.H))
+        pygame.display.set_caption("M   I   N   E   S   W   E   E   P   E   R")
+        self.w = self.W-30 # 775
+        self.h = self.H-100 # 500
+
+        self.screen = pygame.display.set_mode((self.W, self.H))
 
         self.font_board = pygame.font.SysFont(None, 30)
-        self.font_header_text = pygame.font.SysFont("Comic Sans MS", 20)
-        self.font_header_value = pygame.font.SysFont("Comic Sans MS", 36)
+        self.font_header_text = pygame.font.SysFont("Comic Sans MS", 16)
+        self.font_header_value = pygame.font.SysFont("Comic Sans MS", 24)
         self.textE1 = "EASY"
-        self.textE2 = "15x15"
+        self.textE2 = "8x8"
         self.textM1 = "MEDIUM"
-        self.textM2 = "25x25"
-        self.textD1 = "DIFFICULT"
-        self.textD2 = "35x35"
+        self.textM2 = "16x16"
+        self.textD1 = "EXPERT"
+        self.textD2 = "24x24"
         self.author = "Copyright 2019 Stefan Gal"
         self.FONT1 = pygame.font.Font(None, 42)
         self.FONT2 = pygame.font.Font(None, 32)
@@ -44,11 +45,11 @@ class MS:
 
         self.flag_img = pygame.image.load("img/flag.png").convert_alpha()
         self.flag_img = pygame.transform.scale(self.flag_img, (23,23))
-        self.bomb_img = pygame.image.load("img/bomb.png").convert_alpha()
-        self.bomb_img = pygame.transform.scale(self.bomb_img, (21,21))
-        self.BOMB_img = pygame.image.load("img/bomb.png").convert_alpha()
-        self.BOMB_img = pygame.transform.scale(self.BOMB_img, (80,100)) #1: 1.2
-        self.icon_img = pygame.image.load("img/icon.png")
+        self.bomb_img = pygame.image.load("img/bomb.jpg").convert_alpha()
+        self.bomb_img = pygame.transform.scale(self.bomb_img, (24,24))
+        self.BOMB_img = pygame.image.load("img/bomb.jpg").convert_alpha()
+        self.BOMB_img = pygame.transform.scale(self.BOMB_img, (200,200)) #1:1
+        self.icon_img = pygame.image.load("img/icon.jpg")
         self.icon_img = pygame.transform.scale(self.icon_img, (15, 15))
         pygame.display.set_icon(self.icon_img)
 
@@ -61,31 +62,40 @@ class MS:
         self.bomb_list = []
         self.board_list = []
 
-        self.get_boardlist()
+        self.get_boardlist(self.w, self.h)
 
 
-    def introPage(self):
-        self.screen.fill((55,55,0))
+    def intro_page(self):
+        self.screen.fill((51,204,204))
         # MINESWEEPER text
         head = pygame.font.SysFont("Comic Sans MS", 48)
         gamename = head.render("M I N E S W E E P E R", True, (255, 255, 0))
-        x = gamename.get_rect().centerx
-        y = gamename.get_rect().centery
-        self.screen.blit(gamename, (MS.W//2-x, MS.H//5-y))
+        self.screen.blit(gamename, (self.W//2-gamename.get_rect().centerx, self.H//5-gamename.get_rect().centery))
         # BOARD DIFFICULTY
-        self.screen.blit(self.txtE1_surface, (150,400))
-        self.screen.blit(self.txtE2_surface, (155,435))
-        self.screen.blit(self.txtM1_surface, (320,400))
-        self.screen.blit(self.txtM2_surface, (345,435))
-        self.screen.blit(self.txtD1_surface, (500,400))
-        self.screen.blit(self.txtD2_surface, (535,435))
-        self.screen.blit(self.author_surface, (335,580))
-        self.screen.blit(self.BOMB_img, (350, 200))
+        self.screen.blit(self.txtE1_surface, (self.W/4-self.txtE1_surface.get_rect().centerx, 400))
+        self.screen.blit(self.txtE2_surface, (self.W/4-self.txtE2_surface.get_rect().centerx, 435))
+        self.screen.blit(self.txtM1_surface, (self.W/2-self.txtM1_surface.get_rect().centerx, 400))
+        self.screen.blit(self.txtM2_surface, (self.W/2-self.txtM2_surface.get_rect().centerx, 435))
+        self.screen.blit(self.txtD1_surface, (self.W/4*3-self.txtD1_surface.get_rect().centerx, 400))
+        self.screen.blit(self.txtD2_surface, (self.W/4*3-self.txtD2_surface.get_rect().centerx, 435))
+        self.screen.blit(self.author_surface, (self.W/2-self.author_surface.get_rect().centerx, 580))
+        self.screen.blit(self.BOMB_img, (self.W/2-self.BOMB_img.get_rect().centerx, 170))
+        # NUMBER OF MINES
+        bomb_font = pygame.font.SysFont(None, 28)
+        b1 = bomb_font.render("10", True, (0, 0, 0))
+        b2 = bomb_font.render("40", True, (0, 0, 0))
+        b3 = bomb_font.render("99", True, (0, 0, 0))
+        self.screen.blit(b1, (self.W/4-self.bomb_img.get_rect().centerx-15, 462))
+        self.screen.blit(self.bomb_img, (self.W/4-self.bomb_img.get_rect().centerx+15, 460))
+        self.screen.blit(b2, (self.W/2-self.bomb_img.get_rect().centerx-15, 462))
+        self.screen.blit(self.bomb_img, (self.W/2-self.bomb_img.get_rect().centerx+15, 460))
+        self.screen.blit(b3, (self.W/4*3-self.bomb_img.get_rect().centerx-15, 462))
+        self.screen.blit(self.bomb_img, (self.W/4*3-self.bomb_img.get_rect().centerx+15, 460))
 
     def events(self):
-        txtE1pos = self.txtE1_surface.get_rect(topleft=(150, 400))
-        txtM1pos = self.txtM1_surface.get_rect(topleft=(320, 400))
-        txtD1pos = self.txtD1_surface.get_rect(topleft=(500, 400))
+        txtE1pos = self.txtE1_surface.get_rect(topleft=(self.W/4-self.txtE1_surface.get_rect().centerx, 400))
+        txtM1pos = self.txtM1_surface.get_rect(topleft=(self.W/2-self.txtM1_surface.get_rect().centerx, 400))
+        txtD1pos = self.txtD1_surface.get_rect(topleft=(self.W/4*3-self.txtD1_surface.get_rect().centerx, 400))
 
         for event in pygame.event.get():
             if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE or event.type == QUIT:
@@ -111,32 +121,33 @@ class MS:
                 self.txtM1_surface = self.FONT1.render(self.textM1, True, (255, 0, 0))
                 self.txtD1_surface = self.FONT1.render(self.textD1, True, (255, 0, 0))
 
-    def get_boardlist(self):
+    def get_boardlist(self, width, hight):
         """
         Makes a list of coordinates for each cell.
         """
-        for a, x in enumerate(range(15, self.w+15, 25)):
-            for b, y in enumerate(range(90, 580, 25)):
-                self.board_list.append([x,y])
+        self.board_list = []
+        for a, x in enumerate(range(15, width, 25)): #w+15
+            for b, y in enumerate(range(90, hight+75, 25)):
+                self.board_list.append([x, y])
         return self.board_list
 
     def board(self):
         """
         Draws pics based on Array.
         """
-        for a, x in enumerate(range(15, 785, 25)):
-            for b, y in enumerate(range(90, 580, 25)):
+        for a, x in enumerate(range(15, self.w, 25)):
+            for b, y in enumerate(range(90, self.h+75, 25)):
                 if self.np_board.board[a][b] == 0:
-                    pygame.draw.rect(self.screen, MS.WHITE, [x, y, 23,23], 0)
+                    pygame.draw.rect(self.screen, self.WHITE, [x, y, 23,23], 0)
                     self.empty_list.append([x, y])
                 elif self.np_board.board[a][b] > 0:
                     text = self.font_board.render(str(self.np_board.board[a][b]), True, (0, 128, 0))
-                    pygame.draw.rect(self.screen, MS.WHITE, [x, y, 23,23], 0)
+                    pygame.draw.rect(self.screen, self.WHITE, [x, y, 23,23], 0)
                     self.screen.blit(text, (x+5,y+2))
                     self.touching_list.append([x, y])
                 elif self.np_board.board[a][b] == -1:
-                    pygame.draw.rect(self.screen, MS.RED, [x, y, 23,23], 0)
-                    self.screen.blit(self.bomb_img,(x+1, y+1))
+                    pygame.draw.rect(self.screen, self.GREY, [x, y, 23,23], 0)
+                    self.screen.blit(self.bomb_img,(x, y))
                     self.bomb_list.append([x, y])
 
     def cover(self):
@@ -144,10 +155,19 @@ class MS:
         Hides all the cells based on board_list (not clicked).
         """
         for xy in self.board_list:
-            pygame.draw.rect(self.screen, MS.GREY, [xy[0], xy[1], 23,23], 0)
+            pygame.draw.rect(self.screen, self.GREY, [xy[0], xy[1], 23,23], 0)
         for xy in self.flag_list:
-            pygame.draw.rect(self.screen, MS.GREY, [xy[0], xy[1], 23,23], 0)
+            pygame.draw.rect(self.screen, self.GREY, [xy[0], xy[1], 23,23], 0)
             self.screen.blit(self.flag_img, [xy[0], xy[1]])
+
+    # def cover(self):
+    #     """
+    #     Hides all the cells based on board_list (not clicked).
+    #     """
+    #     for a, x in enumerate(range(15, self.w, 25)):
+    #         for b, y in enumerate(range(90, self.h+75, 25)):
+    #             if self.np_board.board[a][b] in [-1,0,1,2,3,4,5,6,7,8,9]:
+    #                 pygame.draw.rect(self.screen, self.GREY, [x, y, 23,23], 0)
 
 
     def clicked(self):
@@ -177,35 +197,41 @@ class MS:
                     self.flag_list.pop(self.flag_list.index([x, y]))
                     self.flags -= 1
 
+    # def timer(self, run=True):
+    #     if run:
+    #         start = timer()
+    #         end = timer()
+    #         tm =timedelta(seconds=end-start)
+    #     return tm
+
     def header(self, surface):
-        move_counter = 620 % len(self.board_list)
 
-        moves_text = self.font_header_value.render("620"+"/"+str(move_counter), True, (255, 0, 0))
-
+        move_counter = (self.w//25 * self.h//25) % len(self.board_list)
+        moves_text = self.font_header_value.render(str(move_counter)+"/"+str(self.np_board.board.size), True, (255, 0, 0))
         bombs_text = self.font_header_text.render("Bombs", True, (255, 0, 0))
         bombs_value = self.font_header_value.render(str(self.bomb_qty), True, (255, 0, 0))
         flags_text = self.font_header_text.render("Flags", True, (255, 0, 0))
         flags_value = self.font_header_value.render(str(self.flags), True, (255, 0, 0))
 
-        surface.blit(moves_text, (650, 15))
+        surface.blit(moves_text, (self.w-(moves_text.get_rect().right), 5))
         surface.blit(bombs_text, (20, 5))
         surface.blit(bombs_value, (20, 25))
-        surface.blit(flags_text, (100, 5))
-        surface.blit(flags_value, (100, 25))
+        surface.blit(flags_text, (80, 5))
+        surface.blit(flags_value, (80, 25))
+
 
     def intro(self):
-        clock = pygame.time.Clock()
+        clock1 = pygame.time.Clock()
         running = True
         while running:
-            clock.tick(60)
+            clock1.tick(60)
             if self.difficulty in [0,1,2]:
                 running = False
-                print(self.difficulty)
                 return self.difficulty
             else:
                 self.events()
 
-            self.introPage()
+            self.intro_page()
 
             pygame.display.update()
 
@@ -213,29 +239,48 @@ class MS:
 
 
     def game(self, difficulty):
-        clock = pygame.time.Clock()
+        clock2 = pygame.time.Clock()
         running = True
-        self.screen.fill((0,0,0))
-        top_screen = self.screen.subsurface(pygame.Rect(0, 0, 805, 80 ))
+
         if difficulty == 0:
             self.bomb_qty = 10
+            self.w = 200
+            self.h = 200
+
         elif difficulty == 1:
-            self.bomb_qty = 20
+            self.bomb_qty = 40
+            self.w = 400
+            self.h = 400
+
         elif difficulty == 2:
-            self.bomb_qty = 30
+            self.bomb_qty = 99
+            self.w = 600
+            self.h = 600
+        self.get_boardlist(self.w, self.h)
+        self.screen = pygame.display.set_mode((self.w+30, self.h+100))
+        self.screen.fill((0,0,0))
+        top_screen = self.screen.subsurface(pygame.Rect(0, 0, self.w+30, 80 ))
+        self.np_board = Array(self.w//25, self.h//25, self.bomb_qty)
 
-        self.np_board = Array(31, 20, self.bomb_qty)
+        start_time = time.time()
 
-        print(self.np_board)
+        minutes = 0
         while running:
-            clock.tick(90)
-            top_screen.fill((50, 20, 50))
+            clock2.tick(60)
+            top_screen.fill((20, 20, 20))
 
             self.board()
             self.cover()
             self.clicked()
 
-            self.header(top_screen)
+            self.header(top_screen )
+            seconds = (time.time() - start_time)
+            if (time.time() - start_time) > 59 == 0:
+                minutes += 1
+                start_time = time.time()
+
+            timing = self.font_header_text.render(str(minutes).zfill(2)+":"+str(int(seconds)).zfill(2), True, (0,255,0))
+            top_screen.blit(timing, (self.w-(timing.get_rect().right), 40))
 
             pygame.display.update()
         pygame.quit()
